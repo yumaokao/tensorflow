1d93d30b6f18d30f64e10fa440c97737778aaa11
wei-che.chang <wei-che.chang@mediatek.com>
2018-01-15 14:16:52 +0800

    comparsion compatible with uint8/float

--- tensorflow/contrib/lite/utils/compare_tflite.cc
+++ tensorflow/contrib/lite/utils/compare_tflite.cc
@@ -48,10 +48,11 @@ void FATAL(const char* format, ...) {
     FATAL("Aborting since tflite returned failure."); \
   }
 
+template<typename T>
 static TfLiteStatus ReshapeInputs(tflite::Interpreter* interpreter,
-                                  const char* batch_xs) {
+                                  const char* batch_xs, T type) {
   cnpy::NpyArray arr = cnpy::npy_load(batch_xs);
-  float* src_data = arr.data<float>();
+  T* src_data = arr.data<T>();
   if (!src_data)
     return kTfLiteError;
 
@@ -66,39 +67,86 @@ static TfLiteStatus ReshapeInputs(tflite::Interpreter* interpreter,
   return kTfLiteOk;
 }
 
+template<typename T>
 static TfLiteStatus PrepareInputs(tflite::Interpreter* interpreter,
-                                  const char* batch_xs) {
+                                  const char* batch_xs, T type) {
   TfLiteTensor* tensor = interpreter->tensor(interpreter->inputs()[0]);
   cnpy::NpyArray arr = cnpy::npy_load(batch_xs);
-  float* dst_data = interpreter->typed_tensor<float>(interpreter->inputs()[0]);
-  float* src_data = arr.data<float>();
+  T* dst_data = interpreter->typed_tensor<T>(interpreter->inputs()[0]);
+  T* src_data = arr.data<T>();
   if (!dst_data || !src_data)
     return kTfLiteError;
 
-  size_t num = tensor->bytes / sizeof(float);
-  float* p = dst_data;
-  float* q = src_data;
+  size_t num = tensor->bytes / sizeof(T);
+  T* p = dst_data;
+  T* q = src_data;
   for (p = dst_data, q = src_data; p < dst_data + num; p++, q++) {
     *p = *q;
   }
   return kTfLiteOk;
 }
 
-static TfLiteStatus ClearOutputs(tflite::Interpreter* interpreter) {
+template<typename T>
+static TfLiteStatus ClearOutputs(tflite::Interpreter* interpreter, T type) {
   TfLiteTensor* tensor = interpreter->tensor(interpreter->outputs()[0]);
-  float* data = interpreter->typed_tensor<float>(interpreter->outputs()[0]);
+  T* data = interpreter->typed_tensor<T>(interpreter->outputs()[0]);
   if (!data)
     return kTfLiteError;
   if (data) {
-    size_t num = tensor->bytes / sizeof(float);
-    for (float* p = data; p < data + num; p++) {
+    size_t num = tensor->bytes / sizeof(T);
+    for (T* p = data; p < data + num; p++) {
       *p = 0;
     }
   }
   return kTfLiteOk;
 }
 
-static TfLiteStatus CompareOutputs(tflite::Interpreter* interpreter,
+static TfLiteStatus CompareOutputs_UINT8(tflite::Interpreter* interpreter,
+                                   const char* batch_ys, bool ignore) {
+  constexpr int kRelativeThreshold = 255; // 1e-2f;
+  constexpr int kAbsoluteThreshold = 2; // 1e-4f;
+
+  TfLiteTensor* tensor = interpreter->tensor(interpreter->outputs()[0]);
+  cnpy::NpyArray arr = cnpy::npy_load(batch_ys);
+  uint8_t* out_data = interpreter->typed_tensor<uint8_t>(interpreter->outputs()[0]);
+  uint8_t* ref_data = arr.data<uint8_t>();
+  if (!out_data) printf("no out_data\n");
+  if (!ref_data) printf("no ref_data\n");
+  if (!out_data || !ref_data)
+    return kTfLiteError;
+
+  TfLiteStatus result = kTfLiteOk;
+  size_t num = tensor->bytes / sizeof(uint8_t);
+  int max_diff = 0;
+  int err_cnt = 0;
+  int minus_cnt = 0;
+  for (size_t idx = 0; idx < num; idx++) {
+    uint8_t computed = out_data[idx];
+    uint8_t reference = ref_data[idx];
+    int diff = std::abs((int)computed - (int)reference);
+    bool error_is_large = false;
+    max_diff = std::max(diff, max_diff);
+    if (std::abs(reference) < kRelativeThreshold) {
+      error_is_large = (diff > kAbsoluteThreshold);
+    } else {
+      error_is_large = (diff > kRelativeThreshold * std::abs(reference));
+    }
+    if (error_is_large) {
+      fprintf(stdout, "output[%d][%zu] did not match %hhu vs reference %hhu\n",
+              0, idx, computed, reference);
+      result = kTfLiteError;
+      err_cnt += 1;
+      if (ignore == false)
+        break;
+    } else if (diff == 1){
+      minus_cnt += 1;
+    }
+  }
+  printf("max diff %d, err: (%d/%d), minus: (%d/%d)\n", max_diff, err_cnt,(int)num, minus_cnt, (int)num);
+  return result;
+}
+
+static TfLiteStatus CompareOutputs_FLOAT(tflite::Interpreter* interpreter,
                                    const char* batch_ys, bool ignore) {
   constexpr double kRelativeThreshold = 1e-2f;
   constexpr double kAbsoluteThreshold = 1e-4f;
@@ -133,8 +181,9 @@ static TfLiteStatus CompareOutputs(tflite::Interpreter* interpreter,
   return result;
 }
 
+template <typename T>
 TfLiteStatus Compare(const char* filename, bool use_nnapi,
-         const char* batch_xs, const char* batch_ys, bool ignore) {
+         const char* batch_xs, const char* batch_ys, bool ignore, string typestr, T type) {
   // Read tflite
   auto model = tflite::FlatBufferModel::BuildFromFile(filename);
   if (!model) FATAL("Cannot read file %s\n", filename);
@@ -150,22 +199,27 @@ TfLiteStatus Compare(const char* filename, bool use_nnapi,
   interpreter->UseNNAPI(use_nnapi);
 
   // Reshape with batch
-  ReshapeInputs(interpreter.get(), batch_xs);
+  ReshapeInputs(interpreter.get(), batch_xs, type);
 
   // Allocate Tensors
   interpreter->AllocateTensors();
 
   // Clear outputs[0]
-  ClearOutputs(interpreter.get());
+  ClearOutputs(interpreter.get(), type);
 
   // Prepare inputs[0]
-  PrepareInputs(interpreter.get(), batch_xs);
+  PrepareInputs(interpreter.get(), batch_xs, type);
 
   // Invoke = Run
   interpreter->Invoke();
 
   // Compare outputs
-  TfLiteStatus result = CompareOutputs(interpreter.get(), batch_ys, ignore);
+  TfLiteStatus result;
+  if (typestr == "UINT8") {
+    CompareOutputs_UINT8(interpreter.get(), batch_ys, ignore);
+  } else {
+    CompareOutputs_FLOAT(interpreter.get(), batch_ys, ignore);
+  }
   printf("Running: %s\n", filename);
   printf("  Result: %s\n", (result == kTfLiteOk) ? "OK" : "FAILED");
 
@@ -176,6 +230,7 @@ int main(int argc, char* argv[]) {
   string tflite_file = "";
   string batch_xs = "";
   string batch_ys = "";
+  string inference_type = "";
   bool use_nnapi = true;
   bool ignore = false;
   std::vector<Flag> flag_list = {
@@ -183,6 +238,7 @@ int main(int argc, char* argv[]) {
 	Flag("batch_xs", &batch_xs, "batch_xs npy file to be set as inputs (Must)"),
 	Flag("batch_ys", &batch_ys, "batch_xy npy file to be compared with outputs (Must)"),
     Flag("use_nnapi", &use_nnapi, "use nn api i.e. 0,1"),
+    Flag("inference_type", &inference_type, "FLOAT or UINT8 comparison"),
     Flag("ignore", &ignore, "ignore error to continue compare all, 0,1"),
   };
   string usage = tensorflow::Flags::Usage(argv[0], flag_list);
@@ -195,8 +251,10 @@ int main(int argc, char* argv[]) {
     LOG(ERROR) << usage;
     return -1;
   }
-
-  Compare(tflite_file.c_str(), use_nnapi, batch_xs.c_str(), batch_ys.c_str(), ignore);
-
+  if (inference_type == "UINT8") {
+    Compare(tflite_file.c_str(), use_nnapi, batch_xs.c_str(), batch_ys.c_str(), ignore, inference_type, (uint8_t)1);
+  } else {
+    Compare(tflite_file.c_str(), use_nnapi, batch_xs.c_str(), batch_ys.c_str(), ignore, inference_type, (float)1.0);
+  }
   return 0;
 }
